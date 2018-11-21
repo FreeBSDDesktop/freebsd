489d05dab1d3a0da4cbddf1a85fc0334769cf738
Johannes Lundberg <johalun0@gmail.com>
2018-11-21 17:21:56 +0000

    Fix dev_err_once macro

--- sys/compat/linuxkpi/common/include/linux/device.h
+++ sys/compat/linuxkpi/common/include/linux/device.h
@@ -184,9 +184,11 @@ show_class_attr_string(struct class *class,
 	    device_printf((dev)->bsddev, fmt, ##__VA_ARGS__)
 
 #define	dev_err_once(dev, ...) do {		\
-	static linux_ratelimit_t __ratelimited;	\
-	if (linux_ratelimited(&__ratelimited))	\
+      static bool __dev_err_once;		\
+      if (!__dev_err_once) {			\
+		__dev_err_once = 1;		\
 		dev_err(dev, __VA_ARGS__);	\
+      }						\
 } while (0)
 
 #define	dev_err_ratelimited(dev, ...) do {	\
