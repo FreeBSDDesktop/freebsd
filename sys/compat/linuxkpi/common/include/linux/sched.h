f6695fec64cdae3e915781ee0af3458f50867172
Johannes Lundberg <johalun0@gmail.com>
2018-12-08 15:13:50 +0000

    Add task group_leader and helper function for checking hecking if process is exiting

--- sys/compat/linuxkpi/common/include/linux/sched.h
+++ sys/compat/linuxkpi/common/include/linux/sched.h
@@ -80,6 +80,7 @@ struct task_struct {
 	int rcu_recurse;
 	int bsd_interrupt_value;
 	struct work_struct *work;	/* current work struct, if set */
+	struct task_struct *group_leader;
 };
 
 #define	current	({ \
@@ -143,6 +144,11 @@ linux_schedule_save_interrupt_value(struct task_struct *task, int value)
 {
 	task->bsd_interrupt_value = value;
 }
+
+bool linux_task_exiting(struct task_struct *task);
+
+#define	current_exiting() \
+	linux_task_exiting(current)
 
 static inline int
 linux_schedule_get_interrupt_value(struct task_struct *task)
